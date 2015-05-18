306b6938e76bc44d29079f241f1a4d73af23d1d9
Eduardo Acha <eduardoacha@gmail.com>
2015-05-18 19:04:34 -0300

    Changed module option to outputModule

--- Patch.py
+++ Patch.py
@@ -8,15 +8,15 @@ from ProbeModules.SSLCertificate import SSLCertificate
 
 parser = argparse.ArgumentParser(description='Process Zmap output')
 parser.add_argument('-o', '--output', help='output file', default=sys.stdout)
-parser.add_argument('-m', '--module', help='set output module. default=csv', default='csv')
+parser.add_argument('-om', '--outputModule', help='set output module. default=csv', default='csv')
 parser.add_argument('-i', '--input', help='input file', default=sys.stdin)
 parser.add_argument('--sslCertificate', help='obtain ssl certificate for each ip', action="store_true")
 args = parser.parse_args()
 
 # Setting output module
-if args.module == 'tsv':
+if args.outputModule == 'tsv':
     output = CSVOutput(delimiter=CSVOutput.TSV_DELIMITER, output_file=args.output)
-elif args.module == 'json':
+elif args.outputModule == 'json':
     output = JsonOutput(output_file=args.output)
 else:
     output = CSVOutput(delimiter=CSVOutput.CSV_DELIMITER, output_file=args.output)
@@ -24,7 +24,6 @@ else:
 # Setting input module
 input = CSVInput(input_file=args.input)
 
-# TODO Implementar threads
 if args.sslCertificate:
     ssl_cert = SSLCertificate(input_module=input, output_module=output)
     ssl_cert.start()
