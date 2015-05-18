0ec62d408dc5df5055ac74a66b20b0f06da04446
Eduardo Acha <eduardoacha@gmail.com>
2015-05-18 18:19:52 -0300

    Created Output module to write output in different formats

---
+++ Output/OutputModule.py
@@ -0,0 +1,10 @@
+import sys
+
+
+class OutputModule:
+
+    def __init__(self, output_file=sys.stdout):
+        if output_file != sys.stdout:
+            self.output_file = open(output_file, mode='w')
+        else:
+            self.output_file = output_file
