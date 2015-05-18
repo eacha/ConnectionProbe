32912eb3296ede8274a20fe7e002d052cd1a64fb
Eduardo Acha <eduardoacha@gmail.com>
2015-05-18 18:22:15 -0300

    Added CSV and TSV output module.

---
+++ Output/CSVOutput.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+from Output.OutputModule import OutputModule
+
+
+class CSVOutput(OutputModule):
+    CSV_DELIMITER = ','
+    TSV_DELIMITER = '\t'
+
+    def __init__(self, delimiter=CSV_DELIMITER, output_file=sys.stdout):
+        OutputModule.__init__(self, output_file=output_file)
+
+        self.writer = csv.writer(self.output_file, delimiter=delimiter)
+        self.printed_header = False
+
+    def write_dict(self, dict):
+        if not self.printed_header:
+            self.printed_header = True
+            self.writer.writerow(dict.keys())
+        self.writer.writerow(dict.values())
+
+    def close(self):
+        self.output_file.close()
