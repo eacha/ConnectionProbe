c2485cf8dd2dc53452f9644fd9717843029be965
Eduardo Acha <eduardoacha@gmail.com>
2015-05-18 18:23:00 -0300

    Added Json output module.

---
+++ Output/JsonOutput.py
@@ -0,0 +1,25 @@
+import json
+import sys
+from Output.OutputModule import OutputModule
+
+
+class JsonOutput(OutputModule):
+
+    def __init__(self, output_file=sys.stdout):
+        OutputModule.__init__(self, output_file=output_file)
+
+        self.first_object = True
+        self.output_file.write('[\n')
+
+    def write_dict(self, dict):
+        if self.first_object:
+            self.first_object = False
+            self.output_file.write(json.dumps(dict))
+        else:
+            self.output_file.write(',\n' + json.dumps(dict))
+
+    def close(self):
+        self.output_file.write(']')
+
+
+
