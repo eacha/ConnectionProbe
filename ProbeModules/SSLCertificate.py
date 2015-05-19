9fb091c260f67e7fd2fe2926b450de237d93a01b
Eduardo Acha <eduardoacha@gmail.com>
2015-05-19 18:41:57 -0300

    Change sslversion from TLSv1 to SSLv23

--- ProbeModules/SSLCertificate.py
+++ ProbeModules/SSLCertificate.py
@@ -20,13 +20,12 @@ class SSLCertificate(threading.Thread):
         while ip is not None:
             try:
                 logger.debug('Try to obtain certificate from %s ', ip)
-                raw_certificate = ssl.get_server_certificate((ip, 443), ssl_version=ssl.PROTOCOL_TLSv1)
+                raw_certificate = ssl.get_server_certificate((ip, 443), ssl_version=ssl.PROTOCOL_SSLv23)
                 certificate = Certificate.Certificate(ip, raw_certificate, OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, raw_certificate))
                 self.output_module.write_dict(certificate.data_dict())
             except ssl.SSLError:
                 logger.info('Error to obtain ssl certificate from %s', ip)
             except Exception, e:
-                #print e
                 logger.warning('Error %s from %s', e, ip)
 
             ip = self.input_module.read()
