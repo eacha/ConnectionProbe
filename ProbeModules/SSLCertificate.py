fc34e77866f61a8bc9c1ad1770e8edcea21876af
Eduardo Acha <eduardoacha@gmail.com>
2015-06-11 22:22:48 -0300

    Added staticmethods to verify the type of the errors and get certificate with TLSv1 method

--- ProbeModules/SSLCertificate.py
+++ ProbeModules/SSLCertificate.py
@@ -14,27 +14,55 @@ class SSLCertificate(threading.Thread):
         self.input_module = input_module
         self.output_module = output_module
 
-    def get_certificate(self, ip, verify):
+    def get_certificate(self, ip, verify, version=SSL.SSLv23_METHOD):
         logger.debug('Try to obtain certificate from %s ', ip)
-        ssl_connection = SSLConnection(ip, 443, verify)
+        ssl_connection = SSLConnection(ip, 443, verify, version)
         certificate = ssl_connection.get_formatted_certificate().data_dict()
         self.output_module.write_dict(certificate)
         ssl_connection.close()
+
+    def get_certificate_tslv1(self, ip):
+        try:
+            self.get_certificate(ip, True, version=SSL.TLSv1_METHOD)
+        except SSL.Error, error:
+            logger.error('Error %s to obtain ssl certificate from %s', error, ip)
+            if (not SSLCertificate.is_connection_reset(error)) and (not SSLCertificate.is_timeout(error)):
+                try:
+                    self.get_certificate(ip, False, version=SSL.TLSv1_METHOD)
+                except Exception, error:
+                    logger.error('Error %s from %s', error, ip)
+        except Exception, error:
+            logger.error('Error %s from %s', error, ip)
+
+
+    @staticmethod
+    def is_tslv1(error):
+        if isinstance(error, list):
+            return 'tlsv1 alert protocol version' in error[0]
+        return False
+
+    @staticmethod
+    def is_timeout(error):
+        return 'ETIMEDOUT' in error
+
+    @staticmethod
+    def is_connection_reset(error):
+        return 'ECONNRESET' in error
 
     def run(self):
         ip = self.input_module.read()
         while ip is not None:
             try:
                 self.get_certificate(ip, True)
-            except SSL.Error, e:
-                logger.error('Error %s to obtain ssl certificate from %s', e, ip)
-                if ('ECONNRESET' not in e) and ('ETIMEDOUT' not in e):
+            except SSL.Error, error:
+                logger.error('Error %s to obtain ssl certificate from %s', error, ip)
+                if (not SSLCertificate.is_connection_reset(error)) and (not SSLCertificate.is_timeout(error)):
                     try:
                         self.get_certificate(ip, False)
-                    except Exception, e:
-                        logger.error('Error %s from %s', e, ip)
-            except Exception, e:
-                logger.error('Error %s from %s', e, ip)
+                    except Exception, error:
+                        logger.error('Error %s from %s', error, ip)
+            except Exception, error:
+                logger.error('Error %s from %s', error, ip)
 
             ip = self.input_module.read()
 
