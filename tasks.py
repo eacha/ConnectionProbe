fcc24013b47d7f7bd1ced989e8fde9ac572cfe4d
Eduardo Acha <eduardoacha@gmail.com>
2015-06-13 23:31:54 -0300

    Testing Celery group method

--- tasks.py
+++ tasks.py
@@ -1,9 +1,13 @@
 from celery import Celery
+from ProbeModules.SSLConnection import SSLConnection
 
 __author__ = 'eduardo'
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task
-def add(x, y):
-    return x + y
+def certificate(ip):
+    ssl_connection = SSLConnection(ip, 443, False)
+    cert = ssl_connection.get_formatted_certificate().data_dict()
+    ssl_connection.close()
+    return cert
